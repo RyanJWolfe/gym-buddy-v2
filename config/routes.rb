d47dd98ebe856016871ff8fd7a4943e3fdbf7202
Ryan Wolfe <xryanwolfe@gmail.com>
2023-09-10 20:04:00 -0700

    Scaffold users controller, nest resources under user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
-  resources :exercise_sets
-  resources :exercises
-  resources :workouts
-  resources :workout_types
+  resources :users do
+    resources :workouts do
+      resources :exercises do
+        resources :exercise_sets
+      end
+    end
+  end
+
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
