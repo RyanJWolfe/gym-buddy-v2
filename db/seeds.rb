80c4c5950b6009388fa8014c8e76ba9bdf68cced
Ryan Wolfe <xryanwolfe@gmail.com>
2023-09-10 18:45:41 -0700

    Create workout types in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create workout types
+workout_types = WorkoutType.create([
+  { name: "Run" },
+  { name: "Bike" },
+  { name: "Swim" },
+  { name: "Strength" },
+  { name: "Yoga" },
+  { name: "Pilates" },
+  { name: "Stretch" },
+  { name: "Walk" },
+  { name: "Hike" },
+  { name: "Climb" },
+  { name: "Other" }
+])
