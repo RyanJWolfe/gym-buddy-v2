0d1c8db40dea5205226e2b2e06560a4fbb6b48eb
Ryan Wolfe <xryanwolfe@gmail.com>
2025-03-10 22:05:57 -0700

    feat: setup react with hello world test component

---
+++ app/javascript/components/HelloWorld.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HelloWorld = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-blue-600">
+        Hello from React!
+      </h1>
+    </div>
+  )
+}
+
+export default HelloWorld 
