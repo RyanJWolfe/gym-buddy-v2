f713de1207c00f828bf8698616e4dba8ba5c1818
Ryan Wolfe <xryanwolfe@gmail.com>
2025-03-13 20:39:04 -0700

    feat: display start and end time for workouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,27 +3,44 @@ module ApplicationHelper
   
   def page_title(title = nil)
     if title.present?
-      "#{title} | GymTracker"
+      "#{title} - GymTracker"
     else
       "GymTracker"
     end
   end
   
   def active_link_class(path)
-    current_page?(path) ? "bg-indigo-700 text-white" : "text-white hover:bg-indigo-700"
+    if current_page?(path)
+      "bg-indigo-800 text-white"
+    else
+      "text-indigo-100 hover:bg-indigo-800 hover:text-white"
+    end
   end
   
   def format_weight(weight)
-    return "-" if weight.nil?
-    
-    formatted = weight.to_f
-    formatted % 1 == 0 ? formatted.to_i : formatted
+    return 0 if weight.nil?
+    weight.round(1)
   end
 
   def format_duration(minutes)
-    return "N/A" if minutes.nil?
+    return "0 min" if minutes.nil? || minutes.zero?
     
-    formatted = minutes.to_f
-    "#{formatted % 1 == 0 ? formatted.to_i : formatted} min"
+    if minutes < 60
+      "#{minutes} min"
+    else
+      hours = (minutes / 60).to_i
+      remaining_minutes = (minutes % 60).to_i
+      
+      if remaining_minutes.zero?
+        "#{hours} hr"
+      else
+        "#{hours} hr #{remaining_minutes} min"
+      end
+    end
+  end
+
+  def format_time(time)
+    return nil if time.nil?
+    time.strftime("%l:%M %p").strip
   end
 end
