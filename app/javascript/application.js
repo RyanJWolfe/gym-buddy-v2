1b2fcefc93dfa7e225239abe1938720212c35d65
Ryan Wolfe <xryanwolfe@gmail.com>
2025-03-13 19:52:11 -0700

    Revert "feat: setup react with hello world test component"
    This reverts commit 0d1c8db40dea5205226e2b2e06560a4fbb6b48eb.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,19 +2,3 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "chartkick/chart.js"
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import HelloWorld from 'components/HelloWorld'
-
-document.addEventListener('turbo:load', () => {
-  const reactRoot = document.getElementById('react-root')
-  if (reactRoot) {
-    ReactDOM.render(
-      <React.StrictMode>
-        <HelloWorld />
-      </React.StrictMode>,
-      reactRoot
-    )
-  }
-})
