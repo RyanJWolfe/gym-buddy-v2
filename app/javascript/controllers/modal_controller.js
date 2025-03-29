7675e325cc72bbbe38d928d78191e60c05e0831f
Ryan Wolfe <xryanwolfe@gmail.com>
2025-03-29 12:43:09 -0700

    feat: close modal when clicking outside of content container

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,37 +1,46 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["container"]
+
   connect() {
     // Prevent scrolling on the body when modal is open
     document.body.style.overflow = 'hidden'
 
+    // Add event listener for clicks outside of container
+    this.clickListener = this.handleClick.bind(this)
+    document.addEventListener('click', this.clickListener)
+
     // Add event listener for escape key
-    this.handleKeydown = this.handleKeydown.bind(this)
-    document.addEventListener('keydown', this.handleKeydown)
+    this.handleKeydownListener = this.handleKeydown.bind(this)
+    document.addEventListener('keydown', this.handleKeydownListener)
   }
 
   disconnect() {
     // Re-enable scrolling when modal is closed
     document.body.style.overflow = ''
 
-    // Remove event listener for escape key
-    document.removeEventListener('keydown', this.handleKeydown)
+    // Remove event listeners
+    document.removeEventListener('keydown', this.handleKeydownListener)
+    document.removeEventListener('click', this.clickListener)
   }
 
   close(event) {
     // Close modal by removing it from the DOM
-    console.log("closing modal")
     this.element.remove()
-  }
-
-  stopPropagation(event) {
-    event.stopPropagation();
   }
 
   handleKeydown(event) {
     // Close modal on escape key
     if (event.key === 'Escape') {
       this.element.remove()
+    }
+  }
+
+  handleClick(event) {
+    // if click is outside of container, close modal
+    if (!this.containerTarget.contains(event.target)) {
+      this.close()
     }
   }
 } 
