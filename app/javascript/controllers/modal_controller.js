6cdc008273cb9668a3df1a523fb437fbfa23cd99
Ryan Wolfe <xryanwolfe@gmail.com>
2025-03-29 12:31:11 -0700

    feat: close modal with escape key

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -4,15 +4,34 @@ export default class extends Controller {
   connect() {
     // Prevent scrolling on the body when modal is open
     document.body.style.overflow = 'hidden'
+
+    // Add event listener for escape key
+    this.handleKeydown = this.handleKeydown.bind(this)
+    document.addEventListener('keydown', this.handleKeydown)
   }
 
   disconnect() {
     // Re-enable scrolling when modal is closed
     document.body.style.overflow = ''
+
+    // Remove event listener for escape key
+    document.removeEventListener('keydown', this.handleKeydown)
   }
 
   close(event) {
     // Close modal by removing it from the DOM
+    console.log("closing modal")
     this.element.remove()
+  }
+
+  stopPropagation(event) {
+    event.stopPropagation();
+  }
+
+  handleKeydown(event) {
+    // Close modal on escape key
+    if (event.key === 'Escape') {
+      this.element.remove()
+    }
   }
 } 
