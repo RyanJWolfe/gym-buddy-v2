f1c1d65da21f3e7d95b5229946ed275f6c9467a9
Ryan Wolfe <xryanwolfe@gmail.com>
2025-11-24 21:24:39 -0700

    fix: remove pagy initializer, not currently using

--- config/initializers/pagy.rb
+++
@@ -1,7 +0,0 @@
-# Load the Pagy::Backend module
-require "pagy/extras/bootstrap"
-require "pagy/extras/overflow"
-
-# Set Pagy::DEFAULT options
-Pagy::DEFAULT[:items] = 15
-Pagy::DEFAULT[:overflow] = :last_page
