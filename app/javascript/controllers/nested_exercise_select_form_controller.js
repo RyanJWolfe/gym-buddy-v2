d0a797d88f4bd388f9fb4c302764fe09691cacd3
Ryan Wolfe <xryanwolfe@gmail.com>
2025-11-29 15:46:50 -0700

    feat: select exercise in modal and update nested form all client side

---
+++ app/javascript/controllers/nested_exercise_select_form_controller.js
@@ -0,0 +1,32 @@
+import RailsNestedForm from "@stimulus-components/rails-nested-form";
+
+// Connects to data-controller="nested-exercise-select-form"
+export default class extends RailsNestedForm {
+  static targets = ["exerciseFormField", "exerciseContainer"];
+
+  connect() {
+    super.connect();
+  }
+
+  addExercise(event) {
+    event.preventDefault();
+
+    // this.add(event)
+    this.newAdd(event);
+  }
+
+  newAdd(e) {
+    e.preventDefault()
+
+    const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime().toString())
+    this.targetTarget.insertAdjacentHTML("beforebegin", content)
+
+    const exerciseName = e.target.dataset.exerciseName;
+    this.exerciseFormFieldTarget.value = e.target.dataset.exerciseId;
+
+    this.exerciseContainerTarget.innerHTML = `<div class="exercise-item"><h3>${exerciseName}</h3></div>`
+
+    const event = new CustomEvent("rails-nested-form:add", {bubbles: true})
+    this.element.dispatchEvent(event)
+  }
+}
