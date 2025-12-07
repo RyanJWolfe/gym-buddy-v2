7396b1f95fe8456ddb8bf7584897898beb416b10
Ryan Wolfe <xryanwolfe@gmail.com>
2025-12-06 17:46:18 -0700

    feat(routine form): display empty state when routine has no exercises

--- app/javascript/controllers/nested_exercise_select_form_controller.js
+++ app/javascript/controllers/nested_exercise_select_form_controller.js
@@ -2,7 +2,7 @@ import RailsNestedForm from "@stimulus-components/rails-nested-form";
 
 // Connects to data-controller="nested-exercise-select-form"
 export default class extends RailsNestedForm {
-  static targets = ["exerciseFormField", "exerciseContainer"];
+  static targets = ["exerciseFormField", "exerciseContainer", "emptyStateContainer"];
 
   connect() {
     super.connect();
@@ -14,21 +14,47 @@ export default class extends RailsNestedForm {
     this.newAdd(event);
   }
 
+  remove(e) {
+    super.remove(e);
+
+    if (this.formExercisesEmpty()) {
+      this.showEmptyState()
+    }
+  }
+
+  formExercisesEmpty() {
+    return document.querySelector(this.wrapperSelectorValue) === null
+  }
+
   // basically what super.add does, but customized to set the exercise id and name on the newly added nested form.
   newAdd(e) {
     e.preventDefault()
 
+    this.removeEmptyState();
+
     const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime().toString())
     this.targetTarget.insertAdjacentHTML("beforebegin", content)
 
-    const exerciseName = e.target.dataset.exerciseName;
-    this.exerciseFormFieldTarget.value = e.target.dataset.exerciseId;
-    // remove target attribute so that new adds don't overwrite this one
-    this.exerciseFormFieldTarget.removeAttribute("data-nested-exercise-select-form-target");
-
-    this.exerciseContainerTarget.outerHTML = `<h5 class="font-medium">${exerciseName}</h5>`
+    this.setExerciseField(e.target.dataset.exerciseId, e.target.dataset.exerciseName);
 
     const event = new CustomEvent("rails-nested-form:add", {bubbles: true})
     this.element.dispatchEvent(event)
+  }
+
+  setExerciseField(exerciseId, exerciseName) {
+    this.exerciseFormFieldTarget.value = exerciseId;
+    this.exerciseContainerTarget.outerHTML = `<h5 class="font-medium">${exerciseName}</h5>`;
+    // remove target attribute so that new adds don't overwrite this one
+    this.exerciseFormFieldTarget.removeAttribute("data-nested-exercise-select-form-target");
+  }
+
+  removeEmptyState() {
+    if (this.hasEmptyStateContainerTarget && !this.emptyStateContainerTarget.classList.contains("hidden")) {
+      this.emptyStateContainerTarget.classList.add("hidden");
+    }
+  }
+
+  showEmptyState() {
+    this.emptyStateContainerTarget.classList.remove("hidden");
   }
 }
