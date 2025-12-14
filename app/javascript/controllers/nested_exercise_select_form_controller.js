8df091a1eb383cf468d41f46af360669887a589a
Ryan Wolfe <xryanwolfe@gmail.com>
2025-12-14 00:06:36 -0700

    feat: add basic confirm dialog when removing exercise from routine

--- app/javascript/controllers/nested_exercise_select_form_controller.js
+++ app/javascript/controllers/nested_exercise_select_form_controller.js
@@ -67,6 +67,8 @@ export default class extends RailsNestedForm {
   }
 
   remove(e) {
+    if (!confirm("Are you sure you want to remove this exercise?")) return
+
     super.remove(e);
 
     if (this.formExercisesEmpty()) {
