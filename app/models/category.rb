b6ccf5384907b541d98a020842ec61318d3d3b97
Ryan Wolfe <xryanwolfe@gmail.com>
2025-12-15 20:01:11 -0700

    feat: define list of categories and validate that category name is in list

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,31 @@
 class Category < ApplicationRecord
+  KEYS = %i[
+    chest
+    shoulders
+    rear_delts
+    triceps
+    lats
+    lower_back
+    upper_back
+    biceps
+    forearms
+    traps
+    quadriceps
+    hamstrings
+    glutes
+    calves
+    abductors
+    adductors
+    neck
+    abdominals
+    cardio
+    other
+  ].freeze
+
+  SUPPORTED_NAMES = KEYS.map { |k| k.to_s.tr("_", " ") }.freeze
+
   has_many :exercise_categories
   has_many :exercises, through: :exercise_categories
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, inclusion: { in: SUPPORTED_NAMES }
 end
