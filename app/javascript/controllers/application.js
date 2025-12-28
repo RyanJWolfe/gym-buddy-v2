381112fe79febedd03490116529ac8d52c706742
Ryan Wolfe <xryanwolfe@gmail.com>
2025-12-28 10:44:04 -0700

    chore(stimulus): register dialog controller from @stimulus-components

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,5 +1,6 @@
 import { Application } from "@hotwired/stimulus"
 import RailsNestedForm from "@stimulus-components/rails-nested-form"
+import Dialog from "@stimulus-components/dialog"
 
 const application = Application.start()
 
@@ -7,5 +8,6 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 application.register("nested-form", RailsNestedForm)
+application.register("dialog", Dialog)
 
 export { application }
