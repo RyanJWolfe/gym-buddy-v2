e20a231af408330116c8b4a269f76150f94f1b84
Ryan Wolfe <xryanwolfe@gmail.com>
2026-01-22 21:56:26 -0700

    db: add duration_seconds to workouts

---
+++ db/migrate/20260123045309_add_duration_seconds_to_workouts.rb
@@ -0,0 +1,5 @@
+class AddDurationSecondsToWorkouts < ActiveRecord::Migration[8.1]
+  def change
+    add_column :workouts, :duration_seconds, :integer, default: 0, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_05_035007) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_23_045309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -126,6 +126,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_05_035007) do
   create_table "workouts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.date "date", null: false
+    t.integer "duration_seconds", default: 0, null: false
     t.datetime "end_time"
     t.integer "exercise_logs_count", default: 0, null: false
     t.string "name", null: false
