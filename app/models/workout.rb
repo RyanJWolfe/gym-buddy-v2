ffea833e4987be250eabeea6d97be2044aca4514
Ryan Wolfe <xryanwolfe@gmail.com>
2026-01-23 00:08:35 -0700

    feat(workout): remove end time validations, column is deprecated

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -11,8 +11,7 @@ class Workout < ApplicationRecord
   accepts_nested_attributes_for :exercise_logs, allow_destroy: true
 
   validates :name, presence: true
-  validates :start_time, :end_time, presence: true, if: completed
-  validate :end_time_after_start_time, if: -> { start_time.present? && end_time.present? }
+  validates :start_time, presence: true, if: completed
 
   scope :recent, -> { order(date: :desc) }
 
@@ -57,14 +56,5 @@ class Workout < ApplicationRecord
     base_template = template || self
     template_name = base_template.template_name || base_template.name
     user.workouts.where("template_name = ? OR id = ?", template_name, base_template.id)
-  end
-
-  private
-
-  def end_time_after_start_time
-    return unless start_time && end_time
-    if end_time <= start_time
-      errors.add(:end_time, "must be after start time")
-    end
   end
 end
