d7e3174179eefd7fde8e321544b6d33fc1386422
Ryan Wolfe <xryanwolfe@gmail.com>
2026-01-25 21:33:45 -0700

    feat(dashboard): add stats section

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -22,7 +22,20 @@ module.exports = {
       },
       colors: {
         body: "#f5f5f5",
-        primary: "#4f46e5",
+        primary: {
+          DEFAULT: "#4f46e5",
+          hover: "#4338ca",
+          focus: "#6366f1",
+          light: "#eef2ff",
+          'light-hover': "#e0e7ff",
+          ring: "#6366f1",
+          outline: "#c7d2fe",
+        },
+        muted: {
+          DEFAULT: "#525252",
+          hover: "#9ca3af",
+          light: "#737373",
+        }
       }
     },
   },
