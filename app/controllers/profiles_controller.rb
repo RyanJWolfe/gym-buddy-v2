bb1082637f8f492aca20cb74a85ea8c60caaf14e
Ryan Wolfe <xryanwolfe@gmail.com>
2026-01-25 19:29:12 -0700

    feat(profiles): start setting up show view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,19 +1,13 @@
 class ProfilesController < ApplicationController
+  before_action :set_user
+
   def show
-    @user = current_user
-    @total_workouts = @user.workouts.count
-    @total_volume = calculate_total_volume
-    @workout_streak = calculate_workout_streak
-    @favorite_exercise = @user.favorite_exercises(1).first
   end
 
   def edit
-    @user = current_user
   end
 
   def update
-    @user = current_user
-
     if @user.update(user_params)
       redirect_to profile_path, notice: "Profile was successfully updated."
     else
@@ -22,6 +16,10 @@ class ProfilesController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = current_user
+  end
 
   def user_params
     params.require(:user).permit(:name, :email)
