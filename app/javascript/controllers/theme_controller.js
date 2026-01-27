dca0840bf08433a1f428130113cc84e9c07080c5
Ryan Wolfe <xryanwolfe@gmail.com>
2026-01-27 16:37:53 -0700

    refactor(theme.js): get rid of helper function was just over engineering tbh

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -1,16 +1,45 @@
-import { Controller } from "@hotwired/stimulus"
-import { lightTheme, darkTheme, systemPreference } from "../helpers/toggle_theme";
+import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
   selectLight() {
-    lightTheme()
+    localStorage.theme = 'light'
+    this.toggleTheme()
   }
 
   selectDark() {
-    darkTheme()
+    localStorage.theme = 'dark'
+    this.toggleTheme()
   }
 
   selectSystem() {
-    systemPreference()
+    localStorage.removeItem('theme')
+    this.toggleTheme()
+  }
+
+  toggleTheme() {
+    const css = document.createElement('style')
+    css.type = 'text/css'
+    css.appendChild(
+        document.createTextNode(
+            `* {
+       -webkit-transition: none !important;
+       -moz-transition: none !important;
+       -o-transition: none !important;
+       -ms-transition: none !important;
+       transition: none !important;
+    }`
+        )
+    )
+    document.head.appendChild(css)
+
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const isDark = localStorage.theme === 'dark' || (!('theme' in localStorage) && prefersDark);
+
+    ['dark', 'theme-dark'].forEach(cls =>
+        document.documentElement.classList.toggle(cls, isDark)
+    );
+
+    const _ = window.getComputedStyle(css).opacity
+    document.head.removeChild(css)
   }
 }
