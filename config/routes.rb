58eb576ca480cb4f9d23da2c08a5da760de97e64
Ryan Wolfe <xryanwolfe@gmail.com>
2026-02-06 00:04:39 -0700

    feat(pwa): enable pwa and add a bunch of icons

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
     member do
       post "duplicate", to: "workouts#create_duplicate"
-      get :complete    # opens the modal / completion form
+      get :complete # opens the modal / completion form
     end
   end
 
@@ -39,11 +39,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Add these routes
   resource :profile, only: [ :show, :edit, :update ]
