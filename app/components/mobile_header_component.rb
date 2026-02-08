bef43c77544db44642e0c4c07687b02ea9b07d83
Ryan Wolfe <xryanwolfe@gmail.com>
2026-02-08 00:30:06 -0700

    feat: add ability for mobile header back button to close modal instead of being a link

--- app/components/mobile_header_component.rb
+++ app/components/mobile_header_component.rb
@@ -3,10 +3,11 @@
 class MobileHeaderComponent < ViewComponent::Base
   renders_one :action
 
-  def initialize(title:, back_href: nil, back_label: nil, aria_label: "Header")
+  def initialize(title:, back_href: nil, back_label: nil, close_modal: false, aria_label: "Header")
     @title = title
     @back_href = back_href
     @back_label = back_label
+    @close_modal = close_modal
     @aria_label = aria_label
   end
 end
